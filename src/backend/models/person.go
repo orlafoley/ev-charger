19a9b14f9826d1e6bc502ff382d967d41110b21c
evlin-rh <evlin@redhat.com>
2025-05-14 16:43:45 +0100

    Added: Booking DB
    also separated a connection to DB. Have a persons.go and bookings.go separated for modularity

--- src/backend/models/person.go
+++ src/backend/models/person.go
@@ -1,44 +1,8 @@
 package models
 
 import (
-	"database/sql"
-	"log"
-
 	_ "github.com/mattn/go-sqlite3"
 )
-
-var db *sql.DB
-
-func ConnectDatabase() error {
-	var err error
-	db, err = sql.Open("sqlite3", "./ev_charger.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// SQL Members Table Query
-	createTableMembers := `DROP TABLE IF EXISTS MEMBERS;
-		CREATE TABLE IF NOT EXISTS MEMBERS (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		username TEXT NOT NULL
-	);`
-
-	_, err = db.Exec(createTableMembers)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	insertSampleDataToMembers := `INSERT INTO MEMBERS(username)
-									VALUES ("evan"),
-											("orla"),
-											("daniel");`
-	_, err = db.Exec(insertSampleDataToMembers)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return nil
-}
 
 type Member struct {
 	Id       int    `json:"id"`
@@ -46,7 +10,7 @@ type Member struct {
 }
 
 func GetAllMember() ([]Member, error) {
-	rows, err := db.Query("SELECT id, username FROM MEMBERS")
+	rows, err := DB.Query("SELECT id, username FROM MEMBERS")
 
 	if err != nil {
 		return nil, err
